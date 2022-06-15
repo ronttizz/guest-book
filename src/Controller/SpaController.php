b9eb60ab799258136db72d496c88dc223b85cdca
ronttizz <toni.nissinen@gmail.com>
2022-06-15 09:00:13 +0300

    Basic structure of index

--- src/Controller/SpaController.php
+++ src/Controller/SpaController.php
@@ -8,11 +8,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SpaController extends AbstractController
 {
-    #[Route('/', name: 'app_index')]
+    #[Route('/', name: 'app_index', methods:["GET"])]
     public function index(): Response
     {
         return $this->render('spa/index.html.twig', [
             'controller_name' => 'SpaController',
         ]);
+    }
+
+    #[Route('/create', name: 'app_create')]
+    public function create(): Response
+    {
+        return $this->redirectToRoute('app_index');
     }
 }
